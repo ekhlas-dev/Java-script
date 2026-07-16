e30c71991a4b960b465d8011a99cf24409675c88
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-16 20:27:28 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,15 @@
+let number = 120;
+if (number <= 100) {
+  console.log("big number");
+} else {
+  console.log("small number");
+}
+
+let age = 20;
+let mony = 700;
+if (age >=8 && mony >= 500) {
+  console.log("move")
+}else{
+    console.log("stay home")
+}
+
