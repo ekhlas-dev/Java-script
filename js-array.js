daf206c848b97155b708e4dc917d7e3389c50db9
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-18 22:03:35 +0600

    push poo shift unshift

---
+++ js-array.js
@@ -0,0 +1,20 @@
+// let fruit = ["kathal","lichue","bedena","mango","olive"]
+// console.log(fruit)
+// fruit[2]="potol"
+// console.log(fruit)
+
+// push pop
+
+// let fruit = ["kathal","lichue","bedena","mango","olive"]
+// fruit.push("goava")
+// console.log(fruit)
+// fruit.pop()
+// console.log(fruit)
+
+// shift unshift
+
+let fruit = ["kathal","lichue","bedena","mango","olive"]
+fruit.shift()
+console.log(fruit)
+fruit.unshift("benena")
+console.log(fruit)
