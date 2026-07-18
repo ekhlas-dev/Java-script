2b034f99c1e868f9515fe81f370a63e928e58092
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-18 23:08:30 +0600

    for of loop

---
+++ loop.js
@@ -0,0 +1,17 @@
+let fruits = ["kathal", "lichue", "bedena", "mango", "olive"];
+// for(let fruit of fruits){
+//     console.log("all fruits are hare:",fruit)
+// }
+
+for(let fruit of fruits){
+    console.log(fruit.indexOf([1]))
+}
+
+
+
+// const length = fruits.length
+// for(let i =0; i<fruits.length;i++){
+//     console.log(i)
+//     const item = fruits[i]
+//     console.log(item)
+// }
