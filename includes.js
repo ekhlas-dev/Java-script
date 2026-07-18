53af6c168a05398f823948bccc6040a5ad72a294
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-18 22:44:18 +0600

    indludes

---
+++ includes.js
@@ -0,0 +1,23 @@
+// let fruit = ["kathal", "lichue", "bedena", "mango", "olive"];
+let newFruits = ["alue", "bagun","potol","vendi"]
+// const findFruits = fruit.includes("bedena");
+// console.log(findFruits)
+// if (fruit.includes("mango")) {
+//   console.log("cholo mango khai");
+// }
+
+// let indexItem = fruit.indexOf("mango");
+// console.log(indexItem);
+
+// const concatFruit = fruit.concat(newFruits)
+// console.log(concatFruit)
+
+let fruit = ["kathal", "lichue", "bedena", "mango", "olive"];
+
+// const sliceFruit = fruit.slice(1)
+// console.log(sliceFruit)
+
+// let joinFruit=fruit.join(", ")
+// console.log(joinFruit)
+
+console.log(Array.isArray(fruit))
