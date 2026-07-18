4f58c455e994f05b8c8764088f76c16a50721af5
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-18 21:03:51 +0600

    loop js

---
+++ jslop.js
@@ -0,0 +1,59 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// let i = 0;
+// while(i<=10){
+//   console.log("inside while llop :",i)
+//   i++
+// }
+
+// let sum = 0;
+// for(let i = 1 ; i<=10; i+=3){
+//   sum = sum+i
+//   console.log(i, sum)
+// }
+
+// for (let i = 10; i >= 0; i-=2) {
+//   console.log(i);
+// }
+
+// for(let i = 0; i<=10;i+=2){
+//   console.log(i)
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i)
+// }
+
+// for (let i = 0; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i >= 1; i-=2) {
+//   console.log(i);
+// }
+
+// for(let i = 0 ; i<=20;i +=3){
+//     console.log(i)
+// }
+
+// for (let i = 20; i <= 30; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 20; i <= 30; i += 2) {
+//   console.log(i);
+// }
+
+// for(let i =20 ; i>=0;i--){
+//   console.log(i)
+// }
+
+for (let i = 20; i >= 0; i--) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
