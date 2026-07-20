07970757a8c510e12a0e535b2d938b2f654221fa
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-20 23:14:41 +0600

    done

---
+++ array.js
@@ -0,0 +1,12 @@
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push("yellow")
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+// fruits.unshift("green")
+// console.log(fruits)
+// fruits.shift()
+// console.log(fruits)
+
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits.at(-1))
