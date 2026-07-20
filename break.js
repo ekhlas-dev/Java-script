a887a4f180aa61e7afc4d1b3aa608c8bc1774ab3
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-21 00:21:52 +0600

    program

--- break.js
+++ break.js
@@ -4,7 +4,6 @@
 // console.log(typeof undefined);
 // console.log(typeof null);
 
-
 // console.log(2**2);
 
 // let x = 20;
@@ -34,6 +33,44 @@
 // console.log(5<2);
 // console.log(5<8);
 
+// console.log(5 ==="5");
 
-console.log(5 ==="5");
+// console.log(5 !== 5);
+
+// let age = 19;
+// console.log(age > 18 && age <30);
+
+// console.log(10 > 20 || 10 < 20);
+
+// console.log(!true);
+// console.log(!false);
+
+// let x = 5
+// // x++
+// // console.log(x);
+// console.log(x++);
+// console.log(x)
+
+// let y = 5;
+// console.log(++y);
+// let x = 8;
+// console.log(--x);
+
+// let age = 18
+// console.log(age > 16);
+
+// let mark = 90;
+// console.log(mark >80 && mark<100);
+
+// let password = "admin";
+// console.log(password === "admi");
+
+let age = 18;
+console.log(age > 16);
+
+let marks = 90;
+console.log(marks > 80 && marks < 100);
+
+let password = "admin";
+console.log(password ==="admin");
 
