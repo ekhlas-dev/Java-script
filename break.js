ed2f91e02bc71c7952d9fc2a240f3b2879df7cd3
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-20 23:50:43 +0600

    done

--- break.js
+++ break.js
@@ -1,5 +1,39 @@
-console.log(typeof "djkhbfvjdf");
-console.log(typeof true);
-console.log(typeof Boolean);
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof "djkhbfvjdf");
+// console.log(typeof true);
+// console.log(typeof Boolean);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// console.log(2**2);
+
+// let x = 20;
+// x -=10;
+// console.log(x)
+
+// let x = 20
+// x *=10
+// console.log(x);
+
+// let x = 20;
+// x /=4;
+// console.log(x);
+
+// let x = 45;
+// x **=2
+// console.log(x)
+
+// let g = 52;
+// g %= 5;
+// console.log(g);
+
+// console.log(5>6);
+
+// console.log(5>3);
+
+// console.log(5<2);
+// console.log(5<8);
+
+
+console.log(5 ==="5");
+
