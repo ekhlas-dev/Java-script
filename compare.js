18568e38420058bc87e12427da7551a492117d02
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-21 22:26:58 +0600

    comparerijom

---
+++ compare.js
@@ -0,0 +1,8 @@
+let book1 = "bangla booi"
+let book2 = "Bangla booi"
+if(book1.toLocaleLowerCase() === book2.toLocaleLowerCase()){
+    console.log("tumi pass")
+}else{
+    console.log("tumi fail");
+    
+}
