eed612b290f20fa2cb639422bfd20067decc551c
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-25 21:18:28 +0600

    problem solving

--- chalange.js
+++ chalange.js
@@ -1,144 +1,144 @@
-// Problem 1: Positive, Negative, or Zero
+Problem 1: Positive, Negative, or Zero
 
-// let num = 85;
-// if (num > 0) {
-//   console.log("Positive");
-// } else if (num < 0) {
-//   console.log("Negative");
-// } else {
-//   console.log("zero");
-// }
+let num = 85;
+if (num > 0) {
+  console.log("Positive");
+} else if (num < 0) {
+  console.log("Negative");
+} else {
+  console.log("zero");
+}
 
-// // Problem 2: Even or Odd
+// Problem 2: Even or Odd
 
-// let number = 15;
-// if (number % 2 !== 0) {
-//   console.log("odd number");
-// } else {
-//   console.log("even number");
-// }
+let number = 15;
+if (number % 2 !== 0) {
+  console.log("odd number");
+} else {
+  console.log("even number");
+}
 
-// let age = 16;
-// if (age >= 18) {
-//   console.log("Age 18 or above can vote.");
-// } else {
-//   console.log("Not Eligible");
-// }
+let age = 16;
+if (age >= 18) {
+  console.log("Age 18 or above can vote.");
+} else {
+  console.log("Not Eligible");
+}
 
-// // Problem 5: Largest Number
+// Problem 5: Largest Number
 
-// let a = 20;
-// let b = 45;
-// let c = 10;
+let a = 20;
+let b = 45;
+let c = 10;
 
-// if (a > b && a > b) {
-//   console.log(a);
-// } else if (b > a && b > c) {
-//   console.log(b);
-// } else {
-//   console.log(c);
-// }
+if (a > b && a > b) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
 
-// // Grade Calculator
+// Grade Calculator
 
-// let marks = 83;
-// if (marks >= 90) {
-//   console.log("A");
-// } else if (marks >= 80) {
-//   console.log("B");
-// } else if (marks >= 70) {
-//   console.log("C");
-// } else if (marks >= 60) {
-//   console.log("D");
-// } else {
-//   console.log("F");
-// }
+let marks = 83;
+if (marks >= 90) {
+  console.log("A");
+} else if (marks >= 80) {
+  console.log("B");
+} else if (marks >= 70) {
+  console.log("C");
+} else if (marks >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
 
-// // Leap Year
+// Leap Year
 
-// let year = 2024;
-// if(year % 400 ===0){
-//     console.log("leap year")
-// }else if(year % 100 === 0){
-//     console.log("not leap year")
-// }else if (year % 4 === 0){
-//     console.log("leap year")
-// }else{
-//     console.log("not leap year")
-// }
+let year = 2024;
+if(year % 400 ===0){
+    console.log("leap year")
+}else if(year % 100 === 0){
+    console.log("not leap year")
+}else if (year % 4 === 0){
+    console.log("leap year")
+}else{
+    console.log("not leap year")
+}
 
-// Simple Calculator
+Simple Calculator
 
-// let num1 = 20;
-// let num2 = 5;
-// let operator = "/";
-// if(operator === "+"){
-//     console.log(num1 + num2)
-// }else if(operator === "-"){
-//     console.log(num1-num2)
-// }else if(operator === "*"){
-//     console.log(num1*num2)
-// }else if(operator ==="/"){
-//     console.log(num1/num2)
-// }else{
-//     console.log("invalid user")
-// }
+let num1 = 20;
+let num2 = 5;
+let operator = "/";
+if(operator === "+"){
+    console.log(num1 + num2)
+}else if(operator === "-"){
+    console.log(num1-num2)
+}else if(operator === "*"){
+    console.log(num1*num2)
+}else if(operator ==="/"){
+    console.log(num1/num2)
+}else{
+    console.log("invalid user")
+}
 
-// let num1 = 20;
-// let num2 = 5;
-// let operator = "/";
-// switch (operator) {
-//   case "+":
-//     console.log(num1 + num2);
-//     break;
-//   case "-":
-//     console.log(num1 - num2);
-//     break;
-//   case "*":
-//     console.log(num1 * num2);
-//     break;
-//   case "/":
-//     console.log(num1 / num2);
-//     break;
-//   default:
-//     console.log("invalid number");
-// }
+let num1 = 20;
+let num2 = 5;
+let operator = "/";
+switch (operator) {
+  case "+":
+    console.log(num1 + num2);
+    break;
+  case "-":
+    console.log(num1 - num2);
+    break;
+  case "*":
+    console.log(num1 * num2);
+    break;
+  case "/":
+    console.log(num1 / num2);
+    break;
+  default:
+    console.log("invalid number");
+}
 
-// Temperature
+Temperature
 
-// let temp = 25;
+let temp = 25;
 
-// if (temp < 14) {
-//   console.log("cold");
-// } else if (temp >= 15 && temp <= 30) {
-//   console.log("normal");
-// } else {
-//   console.log("hot");
-// }
+if (temp < 14) {
+  console.log("cold");
+} else if (temp >= 15 && temp <= 30) {
+  console.log("normal");
+} else {
+  console.log("hot");
+}
 
-// Login System
+Login System
 
-// let username = "admin";
-// let password = "12345";
+let username = "admin";
+let password = "12345";
 
-// if (username === "admin" && password === "12345") {
-//   console.log("Login Successful");
-// } else {
-//   console.log("Invalid Username or Password");
-// }
+if (username === "admin" && password === "12345") {
+  console.log("Login Successful");
+} else {
+  console.log("Invalid Username or Password");
+}
 
-// BMI Calculator
+BMI Calculator
 
-// let weight = 902;
-// let height = 1.75;
-// let BMI = weight / (height * height);
-// let bmiSort = BMI.toFixed(2)
-// if (bmiSort < 18.5) {
-//   console.log("UnderWeight");
-// } else if (bmiSort >= 18.5 && bmiSort <= 24.9) {
-//   console.log("Normal");
-// } else if (bmiSort >= 25 && bmiSort <= 29.9) {
-//   console.log("OverWeight");
-// } else {
-//   console.log("Obese");
-// }
+let weight = 902;
+let height = 1.75;
+let BMI = weight / (height * height);
+let bmiSort = BMI.toFixed(2)
+if (bmiSort < 18.5) {
+  console.log("UnderWeight");
+} else if (bmiSort >= 18.5 && bmiSort <= 24.9) {
+  console.log("Normal");
+} else if (bmiSort >= 25 && bmiSort <= 29.9) {
+  console.log("OverWeight");
+} else {
+  console.log("Obese");
+}
