1d55096c50b1f9368971a68f42ed645ee846b987
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-25 15:17:03 +0600

    switch cse

--- if-else.js
+++ if-else.js
@@ -1,31 +1,107 @@
-let age = 28 ;
-if(age >= 18){
-    console.log("you can vote");
-    
-}else{
-    console.log("you can not votw");
-    
+// let age = 28 ;
+// if(age >= 18){
+//     console.log("you can vote");
+
+// }else{
+//     console.log("you can not votw");
+
+// }
+
+// const number = 10;
+// if(number >= 55){
+//     console.log("10 is gatter then 5");
+// }else{
+//     console.log("10 is less then 5");
+
+// }
+
+// let mark = 55;
+// if(mark >=80){
+//     console.log("A+");
+// }else if(mark >=70 ){
+//     console.log("A");
+// }else if(mark >= 60){
+//     console.log("B");
+// }else if(mark >= 50){
+//     console.log("C");
+// }else if (mark >= 40){
+//     console.log("D");
+// }else{
+//     console.log("Fail");
+// }
+
+let money = 50;
+if (money >= 1000) {
+  console.log("I can bay a phone");
+} else if (money >= 500) {
+  console.log("i can bay a hadephone");
+} else {
+  console.log("save the money");
 }
 
-const number = 10;
-if(number >= 55){
-    console.log("10 is gatter then 5"); 
-}else{
-    console.log("10 is less then 5");
-    
+// nested if
+
+const age = 33;
+const hasLicence = false;
+if (age >= 18) {
+  if (hasLicence === true) {
+    console.log("you can drive a car");
+  } else {
+    console.log("you can not drive a car");
+  }
+} else {
+  console.log("you are not adult");
 }
 
-let mark = 55;
-if(mark >=80){
-    console.log("A+");
-}else if(mark >=70 ){
-    console.log("A");
-}else if(mark >= 60){
+let num = 2;
+
+switch (num) {
+  case 1:
+    console.log("One");
+
+  case 2:
+    console.log("Two");
+
+  case 3:
+    console.log("Three");
+    break;
+
+  default:
+    console.log("Done");
+}
+
+let fruit = "mango";
+switch (fruit) {
+  case "apple":
+    console.log("a");
+    break;
+
+  case "kathal":
+    console.log("k");
+    break;
+
+  case "borioi":
     console.log("B");
-}else if(mark >= 50){
-    console.log("C");
-}else if (mark >= 40){
-    console.log("D");
-}else{
-    console.log("Fail");
+    break;
+
+  case "mango":
+    console.log("mango");
+    break;
+  default:
+    console.log("done");
+}
+
+let nums = 5;
+switch (nums) {
+  case 1:
+    console.log("one");
+    break;
+  case 2:
+    console.log("two");
+    break;
+  case 3:
+    console.log("three");
+    break;
+  default:
+    console.log("done");
 }
