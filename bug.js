3b3cea9d419f40457bf58e74d63e55abd9a4c781
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-28 12:23:06 +0600

    global scope function scope, block scpe

--- bug.js
+++ bug.js
@@ -6,14 +6,104 @@
 // }
 // console.log(checkAdult(155))
 
+// let country = "Bangladesh";
 
-let country = "Bangladesh";
+// function show() {
+//   console.log(country);
+// }
 
-function show() {
-  console.log(country);
+// show();
+
+// console.log(country);
+
+// let age = 26;
+
+// if (true) {
+//   console.log(age);
+// }
+
+// function hello() {
+//   console.log(age);
+// }
+
+// hello();
+
+// function person() {
+//   let name = "Aklas";
+//   console.log(name);
+// }
+
+// person();
+
+// // console.log(name);
+
+function sum() {
+  let a = 10;
+  let b = 20;
+
+  console.log(a + b);
 }
 
-show();
+sum();
 
-console.log(country);
+// console.log(a);
+
+function myFunction() {
+  var a = 10;
+
+  if (true) {
+    var b = 20;
+    let c = 30;
+  }
+
+  console.log(a); // 10
+  console.log(b); // 20
+  console.log(c); // ❌ Error
+}
+
+myFunction();
+
+function test() {
+  const age = 26;
+  console.log(age); // ✅ 26
+}
+ console.log(age);
+
+test();
+
+console.log(age); // ❌ ReferenceError
+
+function demo() {
+  if (true) {
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(b); // ❌ Error
+    console.log(c); // ❌ Error
+  }
+
+  console.log(a); // ✅ 10
+}
+
+demo();
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log(window.a); // ✅ 10
+console.log(window.b); // ❌ undefined
+console.log(window.c); // ❌ undefined
+
+let a = 100;
+
+function one() {
+  function two() {
+    console.log(a);
+  }
+
+  two();
+}
+
+one();
 
