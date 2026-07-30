eb00d2e8abdff0f154114383e1ceb2c0f80cf375
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-30 22:46:24 +0600

    problem 2 done

--- assigment.js
+++ assigment.js
@@ -2,17 +2,39 @@ function matchWinner(teamAGoals, teamBGoals) {
   if (typeof teamAGoals !== "number" || typeof teamBGoals !== "number") {
     return "Invalid";
   }
-
   if (teamAGoals > teamBGoals) {
     return "Team A Won";
   } else if (teamBGoals > teamAGoals) {
     return "Team B Won";
   } else {
-    return "Draw";
+    return "Drow";
   }
 }
-console.log(matchWinner(2, 1)); // Team A Won
-console.log(matchWinner(1, 3)); // Team B Won
-console.log(matchWinner(2, 2)); // Draw
-console.log(matchWinner("3", 2)); // Invalid
-console.log(matchWinner(4, "1")); // Invalid
+// console.log(matchWinner(2, 1));
+// console.log(matchWinner(1, 3));
+// console.log(matchWinner(2, 2));
+// console.log(matchWinner("3", 2));
+
+// Problem : 2
+
+function isElevatorSafe(weights) {
+  if (!Array.isArray(weights)) {
+    return "Invalid";
+  }
+  let totalWeight = 0;
+  for (let i = 0; i < weights.length; i++) {
+    totalWeight += weights[i];
+    // console.log(sum);
+  }
+  if (totalWeight > 400) {
+    return false;
+  } else {
+    return true;
+  }
+  return totalWeight;
+}
+console.log(isElevatorSafe([60, 75, 50]));
+console.log(isElevatorSafe([90, 100, 95, 120]));
+console.log(isElevatorSafe([400]));
+console.log(isElevatorSafe("60,75,50"));
+
