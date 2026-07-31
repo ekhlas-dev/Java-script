9c219a6c69332fdf8bac1e3fd72c6d9b4f8e7dcd
ekhlas-dev <ekhlas.cse.eng@gmail.com>
2026-07-31 11:19:39 +0600

    problem 3 done

--- assigment.js
+++ assigment.js
@@ -24,7 +24,7 @@ function isElevatorSafe(weights) {
   let totalWeight = 0;
   for (let i = 0; i < weights.length; i++) {
     totalWeight += weights[i];
-    // console.log(sum);
+    // console.log(totalWeight);
   }
   if (totalWeight > 400) {
     return false;
@@ -33,8 +33,30 @@ function isElevatorSafe(weights) {
   }
   return totalWeight;
 }
-console.log(isElevatorSafe([60, 75, 50]));
-console.log(isElevatorSafe([90, 100, 95, 120]));
-console.log(isElevatorSafe([400]));
-console.log(isElevatorSafe("60,75,50"));
+// console.log(isElevatorSafe([60, 75, 50]));
+// console.log(isElevatorSafe([90, 100, 95, 120]));
+// console.log(isElevatorSafe([400]));
+// console.log(isElevatorSafe("60,75,50"));
+
+// problem: 3
+
+function calculateAiCost(tokensUsed) {
+  if (typeof tokensUsed !== "number" || tokensUsed < 0) {
+    return "invalid";
+  }
+  if (tokensUsed <= 500) {
+    return 0;
+  }
+  let extraToken = tokensUsed - 500;
+  let chargeableBlock = Math.floor(extraToken / 100);
+  return chargeableBlock * 5;
+}
+console.log(calculateAiCost(300))
+console.log(calculateAiCost(500))
+console.log(calculateAiCost(650))
+console.log(calculateAiCost(1000))
+console.log(calculateAiCost(-10))
+console.log(calculateAiCost("500"))
+
+
 
